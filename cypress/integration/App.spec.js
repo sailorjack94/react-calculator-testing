d0d88571d254f883250fda98cb68078c853a228e
sailorj94 <jackniall9@gmail.com>
2021-05-12 19:05:33 +0100

    Add UI Test Passing

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -6,5 +6,20 @@ describe("Calculator", () => {
   it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
-  })
+  });
+
+  it ('shoud be able to add 1 to 4 and get 5', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_add').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '5')
+  });
+
+
+
+
+
+
+
 })
